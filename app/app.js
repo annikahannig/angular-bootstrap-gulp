071f9d35bc65ad596922fa6f076dcb1d8cabd3b9
Matthias Hannig <matthias@hannig.cc>
2015-04-11 12:43:39 +0200

    added app css, markup and basic js

---
+++ app/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Angular Admin
+ * -------------
+ *
+ * Opinionated component oriented admin interface powered
+ * by angular.
+ *
+ * (c) 2015 Matthias Hannig
+ *
+ */
+
+(function() {
+  
+  // Initialize admin application.
+  var app = angular.module('admin', [
+    'ngAnimate',
+    'ngCookies'
+  ]);
+
+})();
+
