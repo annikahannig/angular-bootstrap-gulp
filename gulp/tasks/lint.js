29e473341fbb11ab8c08dde733cbd6203668ce9c
Matthias Hannig <matthias@hannig.cc>
2015-04-11 12:46:42 +0200

    added lint, cleanup and serve task

---
+++ gulp/tasks/lint.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+  * Task: lint
+  * Two words: code quality! 
+  */
+
+var gulp   = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function(){
+  return gulp.src([
+    'app/**/*.js',
+    '!app/bower_components/**'
+  ])
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'));
+
+});
+
---
+++ gulp/tasks/serve.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/**
+ * Task: serve
+ * -----------
+ *
+ * Starts a local http server (similar to grunt serve)
+ * See '../config.json' for configuration options.
+ *
+ * (c) 2015 Matthias Hannig
+ */
+
+var gulp    = require('gulp');
+var gutil   = require('gulp-util');
+var http    = require('http');
+var express = require('express');
+
+
+// == Register serve task
+gulp.task('serve', function(){
+
+  // Create Express server
+  var app = express();
+
+  // Configure server
+  var server = http.createServer(app);
+
+  server.listen(config.serve.port);
+  server.on('error', function(e){
+    if(e.code === 'EADDRINUSE') {
+      gutil.log("Address is already in use. Is a 'serve' task running?");
+    }
+    else {
+      throw e;
+    }
+  });
+  
+  // Serve static build dir
+  app.use('/', express.static('./build'));
+
+});
+
